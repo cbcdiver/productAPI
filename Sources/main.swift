369882fdb64fdee35957cc8ba8a3c51b97cb72df
Chris Chadillon <chris@chadillon.com>
2017-03-28 22:29:04 -0400

    Cleaning

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,9 +2,22 @@ import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
 
-let productList = ProductList()
+/* ************************************************************************* */
+
+func send(HTTPResponse response:HTTPResponse, withJSON theJSON:[String:Any]) {
+    do {
+        try response.setBody(json: theJSON)
+        response.setHeader(.contentType, value: "application/json")
+        response.completed()
+    } catch {
+        response.setBody(string: "Error handling request: \(error)")
+        response.completed()
+    }
+}
 
 /* ************************************************************************* */
+
+let productList = ProductList()
 
 productList.add(product:Product(number: 123, name: "Name 1", price: 22.00))
 productList.add(product:Product(number: 456, name: "Name 2", price: 50.50))
@@ -24,7 +37,6 @@ var routes = Routes()
 
 /* ************************************************************************* */
 
-
 let routesAndErrors = ["/json/": "This route needs a module.",
                        "/json/products/":"This route needs an action.",
                        "/json/products/delete/":"This route needs a product number to delete"]
@@ -35,37 +47,55 @@ BadRoutes().add(routsAndErrors: routesAndErrors).addBadRoutes(to: &routes)
 
 routes.add(method: .get, uri: "/json/products/all") {
     request, response in
-    do {
-        try response.setBody(json: productList.asDictionary)
-        response.setHeader(.contentType, value: "application/json")
-        response.completed()
-        
-    } catch {
-        response.setBody(string: "Error handling request: \(error)")
-        response.completed()
-    }
+    send(HTTPResponse: response, withJSON: productList.asDictionary)
 }
-/* ************************************************************************* */
 
+/* ************************************************************************* */
 
 routes.add(method: .delete, uri: "/json/products/delete/{number}") {
     (request, response) in
     let productNumber = Int(request.urlVariables["number"]!)!
-    do {
-        if productList.remove(productNumber: productNumber) {
-            let result = ["Result":["message":"true"]]
-            try response.setBody(json: result)
-            response.setHeader(.contentType, value: "application/json")
-            response.completed()
-        } else {
-            let result = ["Result":["message":"false"]]
-            try response.setBody(json: result)
-            response.setHeader(.contentType, value: "application/json")
-            response.completed()
+    send(HTTPResponse: response,
+         withJSON: ["Result":productList.remove(productNumber: productNumber)])
+}
+
+/* ************************************************************************* */
+
+routes.add(method: .post, uri: "/json/add") {
+    (request, response) in
+    let productNumber = request.param(name: "number")
+    let name = request.param(name: "name")
+    let price = request.param(name: "price")
+    
+    var canAdd = true
+    var errorMessage:String!
+    
+    var missingFields = [String]()
+    for field in ["number", "name", "price"] {
+        if request.param(name: field) == nil {
+            missingFields.append(field)
         }
-    } catch {
-        response.setBody(string: "Error Generating JSON response: \(error)")
-        response.completed()
+    }
+    
+    if missingFields.count > 0 {
+        canAdd = false
+        errorMessage = "Can not add, the following fields are missing: " +
+            missingFields.joined(separator: ", ")
+    }
+    
+    if canAdd {
+        var blankFields = [String]()
+        for field in ["number", "name", "price"] {
+            if request.param(name: field) == "" {
+                blankFields.append(field)
+            }
+        }
+        
+        if blankFields.count > 0 {
+            canAdd = false
+            errorMessage = "Can not add, the following fields are blank: " +
+                blankFields.joined(separator: ", ")
+        }
     }
 }
 
